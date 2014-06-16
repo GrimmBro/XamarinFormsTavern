e6dbcc927a39c6649ce150970df44b607b17d04a
GrimmBro <dolphindive79@gmail.com>
2014-06-16 03:08:45 +0100

    Xamarin Forms Contest commit to GitHub.

---
+++ XamarinFormsTavern/XamarinFormsTavern/TavernModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamarinFormsTavern
+{
+    class TavernModel
+    {
+        int goldBalance, goldBet;
+    }
+}
